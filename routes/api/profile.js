6ff34f8aaa28ebd860b1f36a22256ed70ecae065
steel710 <adarshagrawal777@gmail.com>
2019-09-21 14:57:10 +0530

    added route to delete edu, exp, profile and user profile and user got deleted at the same time

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -194,7 +194,7 @@ router.post(
 
     Profile.findOne({ user: req.user.id })
       .then(profile => {
-        const newExp = {
+        const newEdu = {
           school: req.body.school,
           degree: req.body.degree,
           stream: req.body.stream,
@@ -204,12 +204,85 @@ router.post(
           description: req.body.description
         };
 
-        //Add newExp object to education
-        profile.education.unshift(newExp);
+        //Add newEdu object to education
+        profile.education.unshift(newEdu);
 
         profile.save().then(profile => res.json(profile));
       })
       .catch(err => res.status(400).json(err));
+  }
+);
+
+//@Route    DELETE /profile/experience/:exp_id
+//@desc     Delete experience from profile
+//@Access   Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //get index of experience
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        //Splice out of array
+        if (removeIndex > -1) {
+          profile.experience.splice(removeIndex, 1);
+
+          profile.save().then(profile => res.json(profile));
+        } else {
+          errors.education = "Requested experience ID not found";
+          return res.status(404).json(errors);
+        }
+      })
+      .catch(err => res.status(400).json(err));
+  }
+);
+
+//@Route    DELETE /profile/education/:edu_id
+//@desc     Delete education from profile
+//@Access   Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //get index of education
+        const removeIndex = profile.education.map(item => item._id).indexOf(req.params.edu_id);
+
+        //Splice out of array
+        if (removeIndex > -1) {
+          profile.education.splice(removeIndex, 1);
+
+          profile.save().then(profile => res.json(profile));
+        } else {
+          errors.education = "Requested education ID not found";
+          return res.status(404).json(errors);
+        }
+      })
+      .catch(err => res.status(400).json(err));
+  }
+);
+
+//@Route    DELETE /profile
+//@desc     Delete the user and profile
+//@Access   Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+      .then(() => {
+        User.findOneAndRemove({ _id: req.user.id })
+          .then(() => res.json({ success: true }))
+          .catch(err => res.json(err));
+      })
+      .catch(err => res.json(err));
   }
 );
 
