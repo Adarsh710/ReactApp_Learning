e21b81097148878a77668a29881060c0863ec030
Steel710 <adarshagrawal777@gmail.com>
2019-10-04 12:00:00 +0530

    started implementation of redux state managment

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as BR, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from './store';
 
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
@@ -8,19 +11,22 @@ import Landing from "./components/layouts/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+
 function App() {
   return (
-    <BR>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <div>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
+    <Provider store={store}>
+      <BR>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </BR>
+      </BR>
+    </Provider>
   );
 }
 
